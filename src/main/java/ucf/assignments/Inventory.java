b620c271b2afcb357f6bffc8de50aad12921615d
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-07-20 20:34:24 -0400

    Built the sorting functions and tests to confirm their functionality.

--- src/main/java/ucf/assignments/Inventory.java
+++ src/main/java/ucf/assignments/Inventory.java
@@ -1,32 +1,65 @@
 package ucf.assignments;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Inventory {
-    ArrayList<Item> items = new ArrayList();
+    private ArrayList<Item> items = new ArrayList();
 
-    void addItem(Item input){
+    void addItem(Item input) {
         this.items.add(input);
     }
 
-    void removeItem(Item input){
+    void removeItem(Item input) {
         items.remove(input);
     }
 
-    boolean confirmSerialUniqueness(){
+    int getItemIndexByName(String input) {
+        for (int ticker = 0; ticker < items.size(); ticker++) {
+            if (items.get(ticker).getName().compareTo(input) == 0) {
+                return ticker;
+            }
+        }
+
+        return -1;
+    }
+
+    boolean confirmSerialUniqueness() {
         return true;
     }
 
-    void sortByValue(){
-
+    void sortByValue() {
+        for (int ticker = 0; ticker < items.size() - 1; ticker++) {
+            for (int ticker2 = ticker + 1; ticker2 < items.size(); ticker2++) {
+                if (items.get(ticker2).getValue() < items.get(ticker).getValue()) {
+                    Collections.swap(items, ticker, ticker2);
+                }
+            }
+        }
     }
 
-    void sortBySerialNumber(){
-
+    void sortBySerialNumber() {
+        for (int ticker = 0; ticker < items.size() - 1; ticker++) {
+            for (int ticker2 = ticker + 1; ticker2 < items.size(); ticker2++) {
+                String cap1 = items.get(ticker).getSerialNumber().toUpperCase();
+                String cap2 = items.get(ticker2).getSerialNumber().toUpperCase();
+                if (cap1.compareTo(cap2) > 0) {
+                    Collections.swap(items, ticker, ticker2);
+                }
+            }
+        }
     }
 
-    void sortByName(){
-
+    void sortByName() {
+        for (int ticker = 0; ticker < items.size() - 1; ticker++) {
+            for (int ticker2 = ticker + 1; ticker2 < items.size(); ticker2++) {
+                String cap1 = items.get(ticker).getName().toUpperCase();
+                String cap2 = items.get(ticker2).getName().toUpperCase();
+                if (cap1.compareTo(cap2) > 0) {
+                    Collections.swap(items, ticker, ticker2);
+                }
+            }
+        }
     }
 
     void searchBySerialNumber(String input){
@@ -36,5 +69,4 @@ public class Inventory {
     void searchByName(String input){
 
     }
-
 }
---
+++ src/test/java/ucf/assignments/InventoryTest.java
@@ -0,0 +1,62 @@
+package ucf.assignments;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryTest {
+
+    @Test
+    void sortByValueSortsCorrectly(){
+        Item item1 = new Item(2, "14567sd", "Scotch Tape");
+        Item item2 = new Item(399.99, "1234", "iPad Mini");
+        Item item3 = new Item(450.00, "123dfs", "Printer Ink");
+
+        Inventory inventory = new Inventory();
+        inventory.addItem(item2);
+        inventory.addItem(item1);
+        inventory.addItem(item3);
+
+        inventory.sortByValue();
+
+        assertEquals(1, inventory.getItemIndexByName("iPad Mini"));
+        assertEquals(0, inventory.getItemIndexByName("Scotch Tape"));
+        assertEquals(2, inventory.getItemIndexByName("Printer Ink"));
+    }
+
+    @Test
+    void sortByNameSortsCorrectly(){
+        Item item1 = new Item(2, "14567sd", "Scotch Tape");
+        Item item2 = new Item(399.99, "d234", "iPad Mini");
+        Item item3 = new Item(450.00, "6623dfs", "Printer Ink");
+
+        Inventory inventory = new Inventory();
+        inventory.addItem(item2);
+        inventory.addItem(item1);
+        inventory.addItem(item3);
+
+        inventory.sortByName();
+
+        assertEquals(0, inventory.getItemIndexByName("iPad Mini"));
+        assertEquals(1, inventory.getItemIndexByName("Printer Ink"));
+        assertEquals(2, inventory.getItemIndexByName("Scotch Tape"));
+    }
+
+    @Test
+    void sortBySerialSortsCorrectly(){
+        Item item1 = new Item(2, "14567sd", "Scotch Tape");
+        Item item2 = new Item(399.99, "d234", "iPad Mini");
+        Item item3 = new Item(450.00, "6623dfs", "Printer Ink");
+
+        Inventory inventory = new Inventory();
+        inventory.addItem(item2);
+        inventory.addItem(item1);
+        inventory.addItem(item3);
+
+        inventory.sortBySerialNumber();
+        assertEquals(0, inventory.getItemIndexByName("Scotch Tape"));
+        assertEquals(1, inventory.getItemIndexByName("Printer Ink"));
+        assertEquals(2, inventory.getItemIndexByName("iPad Mini"));
+
+    }
+}
