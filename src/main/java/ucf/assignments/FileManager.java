c7c06227404a714e7aa4919715bdf26663d5789f
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-07-23 21:11:26 -0400

    Program can handle HTML Files.

--- src/main/java/ucf/assignments/FileManager.java
+++ src/main/java/ucf/assignments/FileManager.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class FileManager {
+
     static String data = "";
 
     public static Inventory readData(File file) throws FileNotFoundException {
@@ -14,17 +15,52 @@ public class FileManager {
         Scanner in = new Scanner(file);
         in.useDelimiter("\\Z");
         data = in.next();
-        System.out.println(data);
         String extension = name.substring(name.lastIndexOf(".") + 1, name.length());
 
         Inventory inventory = new Inventory();
 
         if(extension.compareTo("tsv") == 0){
-            inventory = readTSV(file);
+            inventory = readTSV();
         }else if(extension.compareTo("html") == 0){
-
+            inventory = readHTML();
         }else if(extension.compareTo("json") == 0){
 
+        }
+        return inventory;
+    }
+
+    public static Inventory readTSV(){
+        Scanner in = new Scanner(data);
+        Inventory inventory = new Inventory();
+        in.useDelimiter("\t|\n");
+        while(in.hasNext()){
+
+            Item item = new Item();
+            item.setName(in.next());
+            item.setSerialNumber(in.next());
+            item.setValue(Double.parseDouble(in.next()));
+            inventory.addItem(item);
+        }
+        return inventory;
+    }
+
+    public static Inventory readHTML(){
+        Inventory inventory = new Inventory();
+        Scanner in = new Scanner(data);
+        in.useDelimiter("<td>|</td>|<td>\n</tr>\n<tr>|</body>\n</html>");
+        in.next();
+        //reads the main body of the file and cuts the extra characters off building the array
+        while(in.hasNext()) {
+            Item item = new Item();
+            String name = in.nextLine();
+            item.setName(name.substring(4, name.lastIndexOf('<')));
+            String serial = in.nextLine();
+            item.setSerialNumber(serial.substring(4, serial.lastIndexOf('<')));
+            String value = in.nextLine();
+            item.setValue(Double.parseDouble(value.substring(4, value.lastIndexOf('<'))));
+            in.nextLine();
+            in.nextLine();
+            inventory.addItem(item);
         }
         return inventory;
     }
@@ -34,9 +70,9 @@ public class FileManager {
         String name = file.getName();
         String extension = name.substring(name.lastIndexOf(".") + 1, name.length());
         if(extension.compareTo("tsv") == 0){
-          formatTSV(items);
+            formatTSV(items);
         }else if(extension.compareTo("html") == 0){
-
+            formatHTML(items);
         }else if(extension.compareTo("json") == 0){
 
         }
@@ -62,25 +98,13 @@ public class FileManager {
     }
 
     public static void formatHTML(Inventory items){
-        String dataHTML = "<!DOCTYPE html>\n<html>\n<head>\n  <meta charset=\"UTF-8\">\n  <title>This Data</title>\n</head>\n<body>\n";
-        formatTSV(items);
-        dataHTML = dataHTML + data;
-        dataHTML = dataHTML + "\n</body>\n</html>";
+        String dataHTML = "<!DOCTYPE html>\n<html>\n<head>\n  <meta charset=\"UTF-8\">\n  <title>This Data</title>\n</head>\n<body>\n<table style=\"width:\">\n  <tr>\n    <th>Name</th>\n    <th>Serial Number</th>\n    <th>Value</th>\n  </tr>\n";
+       for(int ticker = 0; ticker < items.getItems().size(); ticker++){
+           dataHTML = dataHTML + "<tr>\n<td>" +items.getItems().get(ticker).getName() + "</td>\n";
+           dataHTML = dataHTML + "<td>" +items.getItems().get(ticker).getSerialNumber() + "</td>\n";
+           dataHTML = dataHTML + "<td>" +items.getItems().get(ticker).getValueString() + "</td>\n</tr>\n";
+       }
+        dataHTML = dataHTML + "</table>\n</body>\n</html>";
         data = dataHTML;
-    }
-
-    public static Inventory readTSV(File tsv){
-        Scanner in = new Scanner(data);
-        Inventory invent = new Inventory();
-        in.useDelimiter("\t|\n");
-        while(in.hasNext()){
-
-            Item item = new Item();
-            item.setName(in.next());
-            item.setSerialNumber(in.next());
-            item.setValue(Double.parseDouble(in.next()));
-            invent.addItem(item);
-        }
-        return invent;
     }
 }
